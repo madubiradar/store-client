baf623b42a658d33564836bb8fd5a36fd053c748
madubiradar <madu.danappa@gmail.com>
2026-02-18 19:18:47 +0530

    lombok annotation added baseurl was fetched from yml file

--- src/main/java/com/storeScout/StoreScout/config/ClientConfig.java
+++ src/main/java/com/storeScout/StoreScout/config/ClientConfig.java
@@ -1,6 +1,7 @@
 package com.storeScout.StoreScout.config;
 
 import com.storeScout.StoreScout.client.StoreClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -10,12 +11,15 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class ClientConfig {
 
+    @Value("${environment.base-url}")
+    private String baseUrl;
+
     @Bean
     public StoreClient storeClient(RestClient.Builder clientBuilder) {
 
         //Build underlying RestClient with BaseURL
         RestClient restClient = clientBuilder
-                .baseUrl("https://fakestoreapi.com/")
+                .baseUrl(baseUrl)
                 .build();
 
         // Create Adapter using that client
@@ -23,7 +27,6 @@ public class ClientConfig {
 
 
         //Create factory and generate proxy implementation
-
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
                 .builderFor(adapter)
                 .build();
--- src/main/java/com/storeScout/StoreScout/model/Product.java
+++ src/main/java/com/storeScout/StoreScout/model/Product.java
@@ -1,6 +1,15 @@
 package com.storeScout.StoreScout.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Product {
 
     private Integer id;
@@ -15,75 +24,4 @@ public class Product {
 
     private String imageUrl;
 
-    public Product() {
-    }
-
-    public Product(Integer id, String title, Double price, String description, String category, String imageUrl) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-        this.imageUrl = imageUrl;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", price=" + price +
-                ", description='" + description + '\'' +
-                ", category='" + category + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                '}';
-    }
 }
